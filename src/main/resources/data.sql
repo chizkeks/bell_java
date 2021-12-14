2ea90f6bd8dbe07e76e42cd4b18a3bba7ffd1136
Ilya Pavlov <pavlov.ilya.y@gmail.com>
2021-12-14 23:33:45 +0300

    корректировки схемы бд и тестовых данных: имя таблицы с типом документов изменено на Doc_Type и в таблицах Citizenship и Doc_Type поля id переименованы в code

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
-INSERT INTO Citizenship (id, name) VALUES(1, 'Российская федерация');
-INSERT INTO Citizenship (id, name) VALUES(2, 'Соединенные штаты Америки');
-INSERT INTO Citizenship (id, name) VALUES(3, 'Европейский союз');
+INSERT INTO Citizenship (code, name) VALUES(1, 'Российская федерация');
+INSERT INTO Citizenship (code, name) VALUES(2, 'Соединенные штаты Америки');
+INSERT INTO Citizenship (code, name) VALUES(3, 'Европейский союз');
 
-INSERT INTO Document_Type (id, name) VALUES(1, 'Паспорт');
-INSERT INTO Document_Type (id, name) VALUES(2, 'ИНН');
-INSERT INTO Document_Type (id, name) VALUES(3, 'Водительское удостоверение');
+INSERT INTO Doc_Type (code, name) VALUES(1, 'Паспорт');
+INSERT INTO Doc_Type (code, name) VALUES(2, 'ИНН');
+INSERT INTO Doc_Type (code, name) VALUES(3, 'Водительское удостоверение');
 
 INSERT INTO Organization (id, version, name, full_name, inn, kpp, address, phone)
 VALUES (1, 0, 'Яндекс', 'ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ ЯНДЕКС', '3596874681', '405744959', 'ул. Льва Толстого, 16, Москва, 119021', '+71011011010');
@@ -27,21 +27,21 @@ VALUES (4, 0, 'Google München', 'Erika-Mann-Str. 33, 80636 Мюнхен', '+491
 INSERT INTO Office (id, version, name, address, phone, org_id)
 VALUES (5, 0, 'Google Ireland', 'Barrow St, Дублин, Dublin City, Ирландия', '+353111222333', 2);
 
-INSERT INTO Person (id, version, first_name, middle_name, surname, job_title, phone, office_id, citizenship_id)
+INSERT INTO Person (id, version, first_name, middle_name, surname, job_title, phone, office_id, citizenship_code)
 VALUES (1, 0, 'Иван', 'Иванович', 'Иванов', 'Software engineer', '+79231112233', 2, 1);
 
-INSERT INTO Person (id, version, first_name, middle_name, surname, job_title, phone, office_id, citizenship_id)
+INSERT INTO Person (id, version, first_name, middle_name, surname, job_title, phone, office_id, citizenship_code)
 VALUES (2, 0, 'Джон', '', 'Джонсон', 'Product manager', '+14934323423', 4, 2);
 
-INSERT INTO Person (id, version, first_name, middle_name, surname, job_title, phone, office_id, citizenship_id)
+INSERT INTO Person (id, version, first_name, middle_name, surname, job_title, phone, office_id, citizenship_code)
 VALUES (3, 0, 'Франк', 'Ульрих', 'Циммерман', 'Бухгалтер', '+491112223344', 5, 3);
 
-INSERT INTO Document (person_id, doc_type_id, number, issue_date)
+INSERT INTO Document (person_id, doctype_code, number, issue_date)
 VALUES (1, 1, '4323 242345', '2020-05-24');
 
-INSERT INTO Document (person_id, doc_type_id, number, issue_date)
+INSERT INTO Document (person_id, doctype_code, number, issue_date)
 VALUES (2, 1, 'C3154352', '2008-08-02');
 
-INSERT INTO Document (person_id, doc_type_id, number, issue_date)
+INSERT INTO Document (person_id, doctype_code, number, issue_date)
 VALUES (3, 3, '195190-0002', '2004-12-31');
 
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS Person (
-    id              INTEGER                          COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    version         INTEGER NOT NULL                 COMMENT 'Служебное поле hibernate',
-    first_name      VARCHAR(50) NOT NULL             COMMENT 'Имя',
-    middle_name     VARCHAR(50)                      COMMENT 'Отчество',
-    surname         VARCHAR(50) NOT NULL             COMMENT 'Фамилия',
-    job_title       VARCHAR(128)                     COMMENT 'Должность',
-    phone           VARCHAR(15)                      COMMENT 'Номер телефона',
-    is_identified   BOOLEAN DEFAULT false NOT NULL   COMMENT 'Флаг идентификации',
-    office_id       INTEGER                          COMMENT 'Уникальный идентификатор офиса (внешний ключ)',
-    citizenship_id  INTEGER                          COMMENT 'Уникальный идентификатор гражданства (внешний ключ)'
+    id                INTEGER                          COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    version           INTEGER NOT NULL                 COMMENT 'Служебное поле hibernate',
+    first_name        VARCHAR(50) NOT NULL             COMMENT 'Имя',
+    middle_name       VARCHAR(50)                      COMMENT 'Отчество',
+    surname           VARCHAR(50) NOT NULL             COMMENT 'Фамилия',
+    job_title         VARCHAR(128)                     COMMENT 'Должность',
+    phone             VARCHAR(15)                      COMMENT 'Номер телефона',
+    is_identified     BOOLEAN DEFAULT false NOT NULL   COMMENT 'Флаг идентификации',
+    office_id         INTEGER                          COMMENT 'Уникальный идентификатор офиса (внешний ключ)',
+    citizenship_code  INTEGER                          COMMENT 'Уникальный идентификатор гражданства (внешний ключ)'
 );
 COMMENT ON TABLE Person IS 'Пользователь';
 
@@ -38,20 +38,20 @@ CREATE TABLE IF NOT EXISTS Organization (
 COMMENT ON TABLE Organization IS 'Организация';
 
 CREATE TABLE IF NOT EXISTS Citizenship (
-    id        INTEGER                 COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    code      INTEGER                 COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
     name      VARCHAR(100) NOT NULL   COMMENT 'Наименование'
 );
 COMMENT ON TABLE Citizenship IS 'Справочник с гражданствами';
 
-CREATE TABLE IF NOT EXISTS Document_Type (
-    id        INTEGER                 COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+CREATE TABLE IF NOT EXISTS Doc_Type (
+    code      INTEGER                 COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
     name      VARCHAR(100) NOT NULL   COMMENT 'Наименование'
 );
-COMMENT ON TABLE Document_Type IS 'Справочник с типами документов';
+COMMENT ON TABLE Doc_Type IS 'Справочник с типами документов';
 
 CREATE TABLE IF NOT EXISTS Document (
     person_id      INTEGER NOT NULL   COMMENT 'Уникальный идентификатор человека',
-    doc_type_id    INTEGER NOT NULL   COMMENT 'Уникальный идентификатор документа',
+    doctype_code   INTEGER NOT NULL   COMMENT 'Уникальный идентификатор документа',
     number         VARCHAR(25)        COMMENT 'Номер документа',
     issue_date     DATE               COMMENT 'Дата выдачи',
 
@@ -61,19 +61,19 @@ COMMENT ON TABLE Document IS 'Таблица документов';
 
 CREATE INDEX IX_Person_Id ON Person (id);
 ALTER TABLE Person ADD FOREIGN KEY (office_id) REFERENCES Office(id);
-ALTER TABLE Person ADD FOREIGN KEY (citizenship_id) REFERENCES Citizenship(id);
+ALTER TABLE Person ADD FOREIGN KEY (citizenship_code) REFERENCES Citizenship(code);
 
 CREATE INDEX IX_Office_Id ON Office (id);
 ALTER TABLE Office ADD FOREIGN KEY (org_id) REFERENCES Organization(id);
 
 CREATE INDEX IX_Organization_Id ON Organization (id);
 
-CREATE INDEX IX_Citizenship_Id ON Citizenship (id);
+CREATE INDEX IX_Citizenship_Code ON Citizenship (code);
 
-CREATE INDEX IX_Document_Type_Id ON Document_Type(id);
+CREATE INDEX IX_Doc_Type_Code ON Doc_Type(code);
 
 CREATE INDEX IX_User_Document_Id ON Document (person_id);
 ALTER TABLE Document ADD FOREIGN KEY (person_id) REFERENCES Person(id);
 
-CREATE INDEX IX_Document_Type_Document_Id ON Document (doc_type_id);
-ALTER TABLE Document ADD FOREIGN KEY (doc_type_id) REFERENCES Document_Type(id);
+CREATE INDEX IX_Doc_Type_Document_Code ON Document (doctype_code);
+ALTER TABLE Document ADD FOREIGN KEY (doctype_code) REFERENCES Doc_Type(code);
